da0c98b758be4ec25804c53c554a61be6108efad
HagenSchanz <schanzhagen@gmail.com>
2021-10-21 10:34:07 +0200

    added printBill, changed Calculation

--- Taxcalculator/src/taxcalculator/Calculation.java
+++ Taxcalculator/src/taxcalculator/Calculation.java
@@ -18,17 +18,44 @@ public class Calculation {
 		for(int i = 0; i < it.size(); i++){
 			if(it.get(i).getBasicTax() == true) {
 				
-				//sets price to price multiplied with basetaxrate
-				it.get(i).setPrice(it.get(i).getPrice().multiply(this.baseTaxrate));
+				//sets price to price plus product of taxcost (the price multiplied with basetaxrate)
+				it.get(i).setPrice(it.get(i).getPrice().add(it.get(i).getPrice().multiply(this.baseTaxrate)));
 			}
 			
-			if(it.get(i).getExtendedTax() == true) {
+			//system of sales tax works different, this part now in printBill function
+			/*if(it.get(i).getExtendedTax() == true) {
 				
 				//sets price to price multiplied with extendedTaxrate
 				it.get(i).setPrice(it.get(i).getPrice().multiply(this.extendedTaxrate));
-			}
+			}*/
 			
 		}
+		
+	}
+	
+	/*puts out multiple items of same type and price as 1item, 1item, 1item and not as 3items, works this way, maybe change later ->high work on code 
+	for just a bit better programm, could work more easy if DB or JSON is implemented*/
+	public void printBill(ArrayList<Item> it) {
+		
+		BigDecimal finalPrice = new BigDecimal("0.00");
+		
+		calculateFinalPrice(it);
+		
+		for(int i = 0; i < it.size(); i++) {
+			
+			System.out.println(String.format("1 %S at %.2f ", it.get(i).getName(), it.get(i).getPrice()));
+			finalPrice = finalPrice.add(it.get(i).getPrice()); 
+			
+			if(it.get(i).getExtendedTax() == true) {
+				System.out.println(String.format("Sales Taxes: %.2f", it.get(i).getPrice().multiply(extendedTaxrate) ));
+				finalPrice = finalPrice.add(it.get(i).getPrice().multiply(extendedTaxrate));
+			}
+			
+			
+			System.out.println(finalPrice);
+		}
+		
+		
 		
 	}
 	
